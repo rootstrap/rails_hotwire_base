cea850ad86a84fd0f33186a64cf8be912d7d6966
Santiago Bartesaghi <santiago.bartesaghi@rootstrap.com>
2021-01-04 10:48:17 -0300

    Revert "Remove Rails UJS since it's not necessary when using Turbo" (#10)
    This reverts commit ddc78c7d562ebb340494a5e8a565875b010a06ca.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,9 +3,11 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+import Rails from '@rails/ujs';
 import * as ActiveStorage from '@rails/activestorage';
 import '@hotwired/turbo-rails';
 import '../channels';
 import '../controllers';
 
+Rails.start();
 ActiveStorage.start();
