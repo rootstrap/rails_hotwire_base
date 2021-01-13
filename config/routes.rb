1cfb6fca53a64988ab4c8319b818c3029744b35e
Santiago Bartesaghi <santib@hey.com>
2021-01-13 09:21:00 -0300

    Rename Welcome to Dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'welcome#index'
+  root 'dashboard#index'
   devise_for :users
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
