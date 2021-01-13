3bc9c1546f251219d2eb9bb8e550d437865d159e
Santiago Bartesaghi <santib@hey.com>
2021-01-13 11:18:01 -0300

    Activate paranoid in Devise

--- spec/system/users/reset_password_spec.rb
+++ spec/system/users/reset_password_spec.rb
@@ -6,26 +6,29 @@ feature 'User reset password' do
   let!(:user) { create(:user) }
 
   feature 'with an existing email' do
-    scenario 'sends a reset password email' do
+    scenario 'shows message that an email was sent if email address exists' do
       visit new_user_password_path
 
       fill_in 'Email', with: user.email
       click_on 'Send me reset password instructions'
 
-      expect(page).to have_text('You will receive an email with instructions '\
-                                'on how to reset your password in a few minutes')
+      expect(page).to have_text('If your email address exists in our database, '\
+                                'you will receive a password recovery link at your email address '\
+                                'in a few minutes')
       expect(page).to have_current_path(new_user_session_path)
     end
   end
 
   feature 'with an non-existent email' do
-    scenario 'does not send a reset password email' do
+    scenario 'shows message that an email was sent if email address exists' do
       visit new_user_password_path
 
       fill_in 'Email', with: 'non-existing-email@example.com'
       click_on 'Send me reset password instructions'
 
-      expect(page).to have_text('Email not found')
+      expect(page).to have_text('If your email address exists in our database, '\
+                                'you will receive a password recovery link at your email address '\
+                                'in a few minutes')
     end
   end
 end
