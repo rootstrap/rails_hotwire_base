0dff6c15694f0248377d663abcacf6e6864d04f2
Juan Diego Rosales <jdrosales17@gmail.com>
2021-02-03 09:09:05 -0300

    Add Flipper for feature flags

---
+++ config/initializers/flipper.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require 'flipper/ui'
+require 'flipper/adapters/active_record'
+require 'flipper/adapters/active_support_cache_store'
+
+Flipper.configure do |config|
+  config.default do
+    adapter = Flipper::Adapters::ActiveRecord.new
+    cached_adapter = Flipper::Adapters::ActiveSupportCacheStore.new(
+      adapter,
+      Rails.cache,
+      expires_in: 1.hour
+    )
+    Flipper.new(cached_adapter)
+  end
+end
+
+# The list of feature flags the application starts with
+feature_flags = ['test']
+
+def initialize_feature_flags(feature_flags)
+  feature_flags.each do |feature_flag|
+    next if Flipper.exist?(feature_flag)
+
+    Flipper.disable(feature_flag)
+  end
+end
+
+initialize_feature_flags(feature_flags)
