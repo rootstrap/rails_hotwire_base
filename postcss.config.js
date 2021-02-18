11cd581048e73459d94ec618e9f4d55abe579798
Julian Pasquale <julianagustin78@gmail.com>
2021-02-18 10:12:50 -0300

    Fix Webpacker after TailwindCSS installation 🥳

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   plugins: [
     require('tailwindcss'),
-	require('postcss-import'),
+    require('postcss-import'),
     require('postcss-flexbugs-fixes'),
     require('postcss-preset-env')({
       autoprefixer: {
